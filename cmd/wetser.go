2afcf7d0c8d1f8ff18758d1608c401b10d3aad78
ashir <ashir@metrictreelabs.com>
2023-09-10 11:33:53 +0530

    added wetser function

--- cmd/wetser.go
+++ cmd/wetser.go
@@ -4,8 +4,12 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
+	"net/http"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -41,32 +45,56 @@ func init() {
 	// wetserCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-type places struct {
+type Places struct {
 	Name    string `json:"name"`
 	Region  string `json:"region"`
-	Country string `json:"Country"`
+	Country string `json:"country"`
 }
 
 func locationSearch() {
 	fmt.Println("Enter your location")
 	var location string
 	fmt.Scan(&location)
+	client := http.Client{Timeout: time.Duration(5) * time.Second}
 	fullUrl := fmt.Sprintf("https://weatherapi-com.p.rapidapi.com/search.json?q=%s", location)
+	req, err := http.NewRequest("GET", fullUrl, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// if err != nil {
-	// 	return nil, err
-	// }
+	req.Header.Add("x-rapidapi-host", "weatherapi-com.p.rapidapi.com")
+	req.Header.Add("x-rapidapi-key", "c256ecdf6emshcedc5242d337c13p19a862jsnee79af877651")
 
-	defer resp.Body.Close()
+	res, err := client.Do(req)
+	if err != nil {
 
-	responses, _ := io.ReadAll(resp.Body)
+		fmt.Println(err)
+	}
 
-	fmt.Println(string(responses))
-	// var searchResponse []places
+	defer res.Body.Close()
 
-	// if err := json.NewDecoder(resp.Body).Decode(&searchResponse); err != nil {
-	// 	return nil, err
-	// }
+	body, err := io.ReadAll(res.Body)
 
-	// return searchResponse, nil
+	if err != nil {
+		fmt.Println("breaaked")
+	}
+
+	// fmt.Println(string(body))
+
+	var placesInstance []Places
+
+	errr := json.Unmarshal(body, &placesInstance)
+
+	if errr != nil {
+		fmt.Println(errr)
+	} else {
+		// Now, placesInstance contains the data from the JSON response
+		for _, place := range placesInstance {
+			fmt.Println("Name:", place.Name)
+			fmt.Println("Region:", place.Region)
+			fmt.Println("Country:", place.Country)
+			fmt.Println("---")
+		}
+	}
+
 }
